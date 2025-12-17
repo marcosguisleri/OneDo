722f455a99af49611ce454bfe86bf9c9fdf5726d
marcosguisleri <marcos.guisleri.dev@gmail.com>
2025-12-16 22:55:22 -0300

    feat: cria endpoint POST /tasks com validação, criação e retorno 201

---
+++ Dtos/CreateTaskRequest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace one_do.Dtos
+{
+    public class CreateTaskRequest
+    {
+        public string Title { get; set; } = "";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using one_do.Domain;
+using one_do.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -14,5 +15,33 @@ listTask.Add(new TaskItem { Id = Guid.NewGuid(), Title = "Criar endpoints de hea
 listTask.Add(new TaskItem { Id = Guid.NewGuid(), Title = "Criar pasta domain com classe para tarefas", Done = false, CreatedAt = DateTime.UtcNow });
 
 app.MapGet("/tasks", () => listTask);
+
+app.MapPost("/tasks", (CreateTaskRequest request) =>
+{
+  var title = request.Title?.Trim();
+
+  if(string.IsNullOrWhiteSpace(title))
+  {
+    return Results.BadRequest(new { error = "O título é obrigatório!" });
+  }
+
+  if(title.Length < 3 || title.Length > 80)
+  {
+    return Results.BadRequest(new { error = "Número de caracteres inválido!" });
+  }
+
+  var task = new TaskItem
+  {
+    Id = Guid.NewGuid(),
+    Title = title,
+    Done = false,
+    CreatedAt = DateTime.UtcNow
+  };
+
+  listTask.Add(task);
+
+  return Results.Created($"/tasks/`{task.Id}", task);
+  
+});
 
 app.Run();
