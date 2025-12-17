d75f4319eab1828e62d09b8f62bf18d74d4d48fa
marcosguisleri <marcos.guisleri.dev@gmail.com>
2025-12-16 22:22:33 -0300

    feat: adiciona endpoint GET /tasks e modelo TaskItem

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using one_do.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/health", () => new { status = "ok" });
 
 app.MapGet("/health/ready", () => new { status = "ok" });
+
+List<TaskItem> listTask = new List<TaskItem>();
+
+listTask.Add(new TaskItem { Id = Guid.NewGuid(), Title = "Criar endpoints de health para novo projeto", Done = false, CreatedAt = DateTime.UtcNow });
+
+listTask.Add(new TaskItem { Id = Guid.NewGuid(), Title = "Criar pasta domain com classe para tarefas", Done = false, CreatedAt = DateTime.UtcNow });
+
+app.MapGet("/tasks", () => listTask);
 
 app.Run();
